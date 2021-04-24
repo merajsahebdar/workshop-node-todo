a941fc93cc330ee935660ce6e630a4a847eed611
Meraj Sahebdar <itsdrewstiles@gmail.com>
2021-04-24 21:57:42 +0430

    fix(auth) fix oauth configs

--- libs/auth/src/modules/oauth/configs/oauth.config.ts
+++ libs/auth/src/modules/oauth/configs/oauth.config.ts
@@ -7,7 +7,17 @@ export function oauthConfig() {
       google: {
         clientId: process.env.OAUTH_GOOGLE_CLIENT_ID,
         clientSecret: process.env.OAUTH_GOOGLE_CLIENT_SECRET,
-        redirectUri: process.env.OAUTH_GOOGLE_CLIENT_REDIRECT_URI,
+        redirectUri: process.env.OAUTH_GOOGLE_REDIRECT_URI,
+      },
+      github: {
+        clientId: process.env.OAUTH_GITHUB_CLIENT_ID,
+        clientSecret: process.env.OAUTH_GITHUB_CLIENT_SECRET,
+        redirectUri: process.env.OAUTH_GITHUB_REDIRECT_URI,
+      },
+      facebook: {
+        clientId: process.env.OAUTH_FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.OAUTH_FACEBOOK_CLIENT_SECRET,
+        redirectUri: process.env.OAUTH_FACEBOOK_REDIRECT_URI,
       },
     },
   };
